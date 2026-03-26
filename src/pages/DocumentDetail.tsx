6ea7e74c225d7fd6d66cda39c653e1f05f8dc052
thuanedp <37993547+thuanedp@users.noreply.github.com>
2026-03-26 15:06:47 +0700

    Update DocumentDetail.tsx

--- src/pages/DocumentDetail.tsx
+++ src/pages/DocumentDetail.tsx
@@ -8,8 +8,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 // Configure PDF.js worker for Vite
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
   import.meta.url,
 ).toString();
 
